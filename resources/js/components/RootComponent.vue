5f51486d1196ce489749558892a7d83b0e6a11a9
SomaSekimoto <0421.soma@gmail.com>
2021-04-27 07:58:56 +0900

    add: footer & fix: switchTheme function by props

--- resources/js/components/RootComponent.vue
+++ resources/js/components/RootComponent.vue
@@ -1,17 +1,29 @@
 <template>
     <div>
-        <template v-if="!['xs', 'sm'].includes(displaySize) ">
-            <header-component></header-component>
-        </template>
+        <header-component v-if="!isSmall" :darkMode="darkMode" @changeTheme="changeTheme"></header-component>
         <body-component></body-component>
+        <footer-component v-if="isSmall" :darkMode="darkMode" @changeTheme="changeTheme" ></footer-component>
     </div>
 </template>
 <script>
 export default {
-    computed: {
-        displaySize: function(){
-            return this.$vuetify.breakpoint.name
-        }
+    data: () => ({
+        hidden: false,
+        darkMode: false,
+    }),
+    created(){
+        this.darkMode = this.$vuetify.theme.dark;
     },
+    computed: {
+        isSmall: function(){
+            return ['xs', 'sm'].includes(this.$vuetify.breakpoint.name)
+        },
+    },
+    methods: {
+        changeTheme: function() {
+            this.darkMode = !this.darkMode;
+            this.$vuetify.theme.dark = this.darkMode;
+        }
+    }
 };
 </script>
