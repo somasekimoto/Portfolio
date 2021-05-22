9143d0ac4cd0b6b443958179f46b0c02abaac3c7
SomaSekimoto <0421.soma@gmail.com>
2021-05-22 17:58:18 +0900

    fix: @mdi/js & bundle analyzer

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,10 @@
 const mix = require("laravel-mix");
 
+// require("laravel-mix-bundle-analyzer");
+
+// if (!mix.inProduction()) {
+//     mix.bundleAnalyzer();
+// }
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
